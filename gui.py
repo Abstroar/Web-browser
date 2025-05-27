f4b4c70fc4d592ff402a29c36cb8f13e2b9d57fa
Abstroar <115562502+Abstroar@users.noreply.github.com>
2025-05-27 23:37:29 +0530

    2-1 skipped and 2-3 is completed

--- gui.py
+++ gui.py
@@ -1,4 +1,8 @@
 import tkinter
+
+
+
+# from Tkinter import *
 from main import URL
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
@@ -7,31 +11,48 @@ SCROLL_STEP = 100
 class Browser:
     def __init__(self):
         self.window=tkinter.Tk()
+        self.width = WIDTH
+        self.height = HEIGHT
         self.canvas = tkinter.Canvas(
             self.window,
-            width=WIDTH,
-            height=HEIGHT
+            width=self.width,
+            height=self.height
         )
-        self.canvas.pack()
+
+        self.canvas.pack(fill=tkinter.BOTH, expand=True)
         self.scroll = 0
         self.display_list = ""
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
         self.window.bind_all("<MouseWheel>", self.on_mousewheel) ##binds the event globally
+        self.window.bind("<Configure>", self.on_resize)
 
     def draw(self):
         self.canvas.delete("all")
         for x, y, c in self.display_list:
-            if y > self.scroll + HEIGHT: continue
+            if y > self.scroll + self.height: continue
             if y + VSTEP < self.scroll: continue
             self.canvas.create_text(x, y-self.scroll, text=c)
 
-    def load(self,  url_obj):
+    def re_layout(self, text):
+        display_list = []
+        cursor_x, cursor_y = HSTEP, VSTEP
+        for c in text:
+            display_list.append((cursor_x, cursor_y, c))
+            cursor_x += HSTEP
+            if cursor_x >= self.width - HSTEP:
+                cursor_y += VSTEP
+                cursor_x = HSTEP
+
+        return display_list
+
+    def load(self, url_obj):
+        self.url_obj = url_obj
         body = url_obj.request()
         body = body.replace("</p>", "\n")
 
-        text = url_obj.lex(body)
-        self.display_list = layout(text)
+        self.text = url_obj.lex(body)
+        self.display_list = self.re_layout(self.text)
         self.draw()
 
 
@@ -49,19 +70,14 @@ class Browser:
         elif event.delta < 0:
             self.scrolldown(event)
 
+    def on_resize(self,event):
+        width = event.width
+        height = event.height - 10
+        self.width = width
+        self.height = height
+        self.display_list = self.re_layout(self.text)
+        self.draw()
 
-def layout(text):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
-            cursor_x = HSTEP
-
-    return display_list
 
 if __name__ == "__main__":
     import sys
